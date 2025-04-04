d85b968ee705e737969d714214d051580af3de86
Sienna <sgoldenbird@gmail.com>
2025-04-04 22:04:05 +0900

    solve: 몫 구하기

---
+++ sgoldenbird/level_0/나이_출력.js
@@ -0,0 +1,8 @@
+/* 
+date: 2025-04-04
+category: math
+difficulty: level 0
+source: Programmers
+number: 120820
+link: https://school.programmers.co.kr/learn/courses/30/lessons/120820
+*/
--- sgoldenbird/level_0/문자열_출력하기.js
+++
@@ -1,8 +0,0 @@
-/* 
-date: 2025-04-02
-category: string
-difficulty: level 0
-source: Programmers
-number: 181952
-link: https://school.programmers.co.kr/learn/courses/30/lessons/181952
-*/
