951fb024953743cda72a726c4b5dca4ce4a0b678
Sienna <sgoldenbird@gmail.com>
2025-04-04 22:06:11 +0900

    solve: 나이 출력

--- sgoldenbird/level_0/나이_출력.js
+++ sgoldenbird/level_0/나이_출력.js
@@ -6,3 +6,8 @@ source: Programmers
 number: 120820
 link: https://school.programmers.co.kr/learn/courses/30/lessons/120820
 */
+function solution(age) {
+  var answer = 0;
+  answer = 2022 - (age - 1);
+  return answer;
+}
