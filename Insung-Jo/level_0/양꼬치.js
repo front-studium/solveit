4ab856a84b2f37e455e4c1637ee7bd61f0e4cc9f
조인성 <leeming01@naver.com>
2025-05-24 13:45:49 +0900

    fix: 일부 조건 수정

--- Insung-Jo/level_0/양꼬치.js
+++ Insung-Jo/level_0/양꼬치.js
@@ -5,11 +5,11 @@ function solution(skewerCount, drinkCount) {
   const NO_SERVICE_COUNT = 0;
 
   if(typeof skewerCount !== 'number' || typeof drinkCount !== 'number') throw new Error('숫자를 입력해주세요.');
-  if (n < 0 || k < 0) throw new Error('음수는 허용되지 않습니다.');
+  if(skewerCount < 0 || drinkCount < 0) throw new Error('음수는 허용되지 않습니다.');
 
-  const drinkService = skewerCount >= FREE_DRINK_THRESHOLD ? Math.trunc(drinkCount / FREE_DRINK_THRESHOLD) : NO_SERVICE_COUNT; 
+  const drinkService =  skewerCount >= FREE_DRINK_THRESHOLD ? Math.trunc(skewerCount / FREE_DRINK_THRESHOLD) : NO_SERVICE_COUNT;
 
-  const total = skewerCount * LAMB_SKEWER_PRICE + drinkCount * DRINK_PRICE - drinkService * DRINK_PRICE;
+  const result = skewerCount * LAMB_SKEWER_PRICE + drinkCount * DRINK_PRICE - drinkService * DRINK_PRICE;
 
-  return total;
+  return result;
 }
